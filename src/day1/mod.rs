87d3c1af514ec67391086176a0b6b71711546369
Giulio Mazzanti <gmazzanti@protonmail.com>
2022-12-02 20:54:29 +0100

    feat(1): implement day1

---
+++ src/day1/mod.rs
@@ -0,0 +1,37 @@
+fn get_groups() -> Vec<u32> {
+    let lines = include_str!("./input.txt").lines();
+    let mut list = Vec::<u32>::new();
+    lines.fold(0, |acc, line| {
+        if let Ok(num) = line.parse::<u32>() {
+            acc + num
+        } else {
+            list.push(acc);
+            0
+        }
+    });
+    list
+}
+
+fn first_star() -> u32 {
+    let groups = get_groups();
+    let min_value = groups.iter().max();
+    match min_value {
+        Some(val) => *val,
+        None => 0,
+    }
+}
+
+fn second_star() -> u32 {
+    let mut groups = get_groups();
+    groups.sort();
+    groups.reverse();
+    groups.iter().take(3).sum()
+}
+
+pub fn day1() {
+    println!(
+        "1st day - First star: {}, Second star: {}",
+        first_star(),
+        second_star()
+    );
+}
---
+++ src/main.rs
@@ -0,0 +1,5 @@
+pub mod day1;
+
+fn main() {
+    day1::day1();
+}
