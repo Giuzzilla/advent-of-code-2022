ae4eb0f741ce8a55f891289a4be44cd1af8f676c
Giulio Mazzanti <gmazzanti@protonmail.com>
2022-12-04 10:42:03 +0100

    feat(4): implement day 4

---
+++ src/day4/mod.rs
@@ -0,0 +1,94 @@
+use std::collections::HashSet;
+
+type Range = (u32, u32);
+
+struct RangePair {
+    first: Range,
+    second: Range,
+}
+
+type RangeSet = HashSet<u32>;
+
+fn get_ranges() -> Vec<RangePair> {
+    include_str!("input.txt")
+        .lines()
+        .map(|line| {
+            let (first_range, second_range) = match line
+                .split(",")
+                .take(2)
+                .collect::<Vec<&str>>()[..]
+            {
+                [first, second] => (first, second),
+                _ => panic!("Invalid input"),
+            };
+
+            let (start_1, end_1) = match first_range
+                .split("-")
+                .take(2)
+                .map(|s| s.parse::<u32>().unwrap())
+                .collect::<Vec<u32>>()[..]
+            {
+                [start, end] => (start, end),
+                _ => panic!("Invalid input"),
+            };
+
+            let (start_2, end_2) = match second_range
+                .split("-")
+                .take(2)
+                .map(|s| s.parse::<u32>().unwrap())
+                .collect::<Vec<u32>>()[..]
+            {
+                [start, end] => (start, end),
+                _ => panic!("Invalid input"),
+            };
+            RangePair {
+                first: (start_1, end_1),
+                second: (start_2, end_2),
+            }
+        })
+        .collect()
+}
+
+fn get_sets(range: RangePair) -> (RangeSet, RangeSet) {
+    let first_set: HashSet<u32> = HashSet::from_iter(range.first.0..=range.first.1);
+    let second_set: HashSet<u32> = HashSet::from_iter(range.second.0..=range.second.1);
+    (first_set, second_set)
+}
+
+fn get_intersection<'a>(first: &RangeSet, second: &RangeSet) -> RangeSet {
+    first.intersection(&second).cloned().collect::<RangeSet>()
+}
+
+fn first_star() -> u32 {
+    let ranges = get_ranges();
+    let mut count: u32 = 0;
+    for range in ranges {
+        let (first_set, second_set) = get_sets(range);
+        let intersection = get_intersection(&first_set, &second_set);
+        if (intersection == first_set) || (intersection == second_set) {
+            count += 1;
+        };
+    }
+    count
+}
+
+fn second_star() -> u32 {
+    let ranges = get_ranges();
+    let mut count: u32 = 0;
+    for range in ranges {
+        let (first_set, second_set) = get_sets(range);
+        let intersection = get_intersection(&first_set, &second_set);
+        if intersection.len() > 0 {
+            count += 1;
+        };
+    }
+    count
+}
+
+pub fn day4() {
+    println!(
+        "Day 4 - First star: {}, Second star: {}",
+        first_star(),
+        second_star()
+    );
+}
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::io;
 pub mod day1;
 pub mod day2;
 pub mod day3;
+pub mod day4;
 
 fn main() {
     let mut day_str = String::new();
@@ -16,6 +17,7 @@ fn main() {
             1 => day1::day1(),
             2 => day2::day2(),
             3 => day3::day3(),
+            4 => day4::day4(),
             _ => println!("Day {} not implemented yet", day),
         },
         _ => println!("Invalid day, must be an integer between 1 and 25"),
